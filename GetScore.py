c7d3dc45bcc072ed32681a7e48d72759f1930b36
TofuNotHere <TofuNotHere@users.noreply.github.com>
2016-05-13 12:02:50 +0800

    修正bug
    之前出了一些奇怪的bug，现在修正一下

--- GetScore.py
+++ GetScore.py
@@ -44,17 +44,22 @@ def initOpener():
     cookie = cookiejar.CookieJar()
     opener=request.build_opener(request.HTTPCookieProcessor(cookie))
     opener.addheaders = [
-        ('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
-        ,('Connection','keep-alive')
-        ,('Origin', 'http://202.115.47.141')
-        ,('DNT','1')
-        ,('Content-Length','197')
-        ,('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36')]
+       ('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+    ,('Cache-Control','max-age=0')
+    ,('Host','202.115.47.141')
+    ,('Connection','keep-alive')
+    ,('Origin', 'http://202.115.47.141')
+    ,('DNT','1')
+    ,('Content-Length','197')
+    ,('Upgrade-Insecure-Requests','1')
+    ,('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36')]
     return opener
 
 
 
-url = 'http://202.115.47.141/bxqcjcxAction.do?totalrows=15&pageSize=300'
+Score_url = 'http://202.115.47.141/bxqcjcxAction.do'
+Score_data = parse.urlencode([('totalrows','300'),
+    ('pageSize', '300')])
 login_url = 'http://202.115.47.141/loginAction.do'
 
 try:
@@ -62,9 +67,10 @@ try:
     login_data = initLoginData()
     ht = opener.open(login_url,login_data.encode('gbk')) #登陆并获取cookie
     #print(ht.read().decode('gbk'))#debug测试
-    htmldoc = opener.open(url)  #获取列表
+    htmldoc = opener.open(Score_url,Score_data.encode('gbk'))  #获取列表
+    #print(htmldoc.read().decode('gbk'))
     showScore(getScore(htmldoc))
-   # print(tmp)
+    #print(tmp)
 except:
-        print('失败了 ╮(╯▽╰)╭')
-#input('Press <enter> to exit!')
+    print('失败了 ╮(╯▽╰)╭')
+input('Press <enter> to exit!')
